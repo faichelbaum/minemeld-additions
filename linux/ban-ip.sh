aca68af7277a275cadd0fecf42d250cd9d8d7430
Francois Aichelbaum <francois@aichelbaum.com>
2020-09-25 13:07:16 +0200

    Update ban-ip.sh

--- linux/ban-ip.sh
+++ linux/ban-ip.sh
@@ -21,6 +21,7 @@ re='^(0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.){3}'
 if [[ $IP =~ $re ]]; then
     curl -XPOST -H "Content-Type: application/json" -u $USER:$PASSWD "http://$FQDN/config/data/${BL}_indicators/append?h=${BL}&t=localdb" -d '{ "indicator": "'$IP'", "type": "IPv4", "share_level": "green", "confidence": 100, "ttl": '$TTL' }'
     ufw deny from $IP to any
+    ufw reload
 else
     usage
     exit 1
