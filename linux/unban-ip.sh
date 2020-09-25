b909014c3e0b672b0fdb70bbcd8acbdea006fe3b
Francois Aichelbaum <francois@aichelbaum.com>
2020-09-25 13:07:29 +0200

    Update unban-ip.sh

--- linux/unban-ip.sh
+++ linux/unban-ip.sh
@@ -21,6 +21,7 @@ re='^(0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.){3}'
 if [[ $IP =~ $re ]]; then
     curl -XPOST -H "Content-Type: application/json" -u $USER:$PASSWD "http://$FQDN/config/data/${BL}_indicators/append?h=${BL}&t=localdb" -d '{ "indicator": "'$IP'", "type": "IPv4", "ttl": 0 }'
     ufw show added | awk '/'$IP'/{ gsub("ufw","ufw delete",$0); system($0)}'
+    ufw reload
 else
     usage
     exit 1
